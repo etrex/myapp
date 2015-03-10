837269538955459b0d21c08f9edb178a48568fd2
etrex <et284vu065k3@gmail.com>
2015-03-10 15:18:47 +0800

    first commit

---
+++ ksana.js
@@ -0,0 +1,48 @@
+jsonp_handler({
+ "version": "1",
+ "build": 2,
+ "title": "myapp",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/etrex/myapp/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css",
+  "sample.kdb"
+ ],
+ "filesizes": [
+  547,
+  64,
+  652507,
+  145170,
+  277980,
+  380,
+  35607,
+  113502,
+  97542
+ ],
+ "filedates": [
+  "2015-03-10T07:00:49.371Z",
+  "2015-03-10T07:00:49.369Z",
+  "2015-03-10T07:00:49.441Z",
+  "2015-03-10T07:02:11.822Z",
+  "2015-03-10T07:00:49.403Z",
+  "2015-03-10T07:00:49.405Z",
+  "2015-03-10T07:00:49.348Z",
+  "2015-03-10T07:00:49.289Z",
+  "2015-03-10T07:02:03.998Z"
+ ],
+ "date": "2015-03-10T07:02:12.791Z"
+})
